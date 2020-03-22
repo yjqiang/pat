ce9f953b64155c6944de682c00b3468b9daa8fc3
yjqiang <yjqiang1@gmail.com>
2020-03-22 12:43:55 +0800

    a1128 八皇后，注意判定公式对角线这里

---
+++ A/1128/main_1.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+#define N_MAX 1001
+int K, N;
+
+// index从1开始
+int nums[N_MAX];
+
+bool isOk() {
+	int i, j;
+	for (i = 1; i <= N; ++i)
+		for (j = i + 1; j <= N; ++j)
+			// nums[i] == nums[j]是看看有没有相同行
+
+			// 对角线/方向时， y = x + b => nums[i] - i == nums[j] - j
+			// 对角线\方向时， y = -x + b => nums[i] + i == nums[j] + j
+			// 所以 nums[i]-nums[j] == ±(i - j)
+			if (nums[i] == nums[j] || abs(nums[i] - nums[j]) == abs(i - j))
+				return false;
+	return true;
+}
+
+int main() {
+	scanf("%d", &K);
+
+	int i, h;
+	for (i = 0; i < K; ++i) {
+		scanf("%d", &N);
+		for (h = 1; h <= N; ++h)
+			scanf("%d", nums + h);
+		if (isOk())
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+
+
+	system("pause");
+	return 0;
+}
