bdf12d3cceeccbb16257192220baf606ca24a801
yjqiang <yjqiang1@gmail.com>
2020-03-23 22:25:11 +0800

    a1138.0b  优化一下，不用全部遍历

---
+++ A/1138/main_0b.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+#define N_MAX 50000
+
+int pre[N_MAX];
+int mid[N_MAX];
+
+// root 为子树根在pre的下标
+// [start, end] 为子树在mid的下标范围
+void post(int root, int start, int end) {
+	int i;
+	for (i = start; mid[i] != pre[root]; ++i);
+	// 有左子树，则post序列第一个元素一定在左子树里面
+	if(start <= i -1)
+		post(root + 1, start, i - 1);
+	// 否则，若有右子树，则一定在右子树
+	else if (i + 1 <= end)
+		post(root + i - start + 1, i + 1, end);
+	// 否则，父节点即为所求
+	else
+		printf("%d", pre[root]);
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	int i;
+	for (i = 0; i < N; ++i)
+		scanf("%d", pre + i);
+	for (i = 0; i < N; ++i)
+		scanf("%d", mid + i);
+
+	post(0, 0, N - 1);
+	system("pause");
+	return 0;
+}
