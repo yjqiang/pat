aef17ec85d76ad02641500caa24de73b7da9aeff
yjqiang <yjqiang1@gmail.com>
2020-03-31 21:40:58 +0800

    二轮微调

--- A/1003/main_1.cpp
+++ A/1003/main_1.cpp
@@ -1,98 +1,121 @@
-# define _CRT_SECURE_NO_WARNINGS
-# include <cstring>
-# include <cstdio>
-# include <cstdlib>
-# include <iostream>
-# include <algorithm>
-# include <climits>
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
+#define N_MAX 500
 
-# define MAX_N 510
+// 邻接矩阵
+// INT_MAX 表示无边
+int edges[N_MAX][N_MAX];
+// 记录每个点的teams数目
+int num_teams[N_MAX];
 
-int city_hands[MAX_N] = {0};
-int edge[MAX_N][MAX_N];
-int dist[MAX_N];
-bool is_in_S[MAX_N] = { false };
-int result_hands[MAX_N] = { 0 };
-int result_count[MAX_N] = { 0 };
 
-int main(){
+// dijkstra使用
+// 是否已经收入S里面
+bool is_in_set[N_MAX];
+// 最短路径条数
+int path_counts[N_MAX];
+// 记录最短路径长度
+int path_dists[N_MAX];
+// start到本点的最短路径上的最大team数目
+int path_num_teams[N_MAX];
+
+
+int main() {
 	int N, M, C1, C2;
 	scanf("%d %d %d %d", &N, &M, &C1, &C2);
-	
-	int i, j;
+
+	int i, h;
 	for (i = 0; i < N; ++i)
-		for (j = 0; j < N; ++j) {
-			if (i == j)
-			{
-				edge[i][j] = 0;
-			}
-			else
-			{
-				edge[i][j] = INT_MAX;
-			}
-		}
+		scanf("%d", num_teams + i);
 
 	for (i = 0; i < N; ++i)
-		dist[i] = INT_MAX;
-			
+		for (h = 0; h < N; ++h)
+			// 清理所有边
+			edges[i][h] = INT_MAX;
 
-
-	///////////////////////////////////////////////////////////////////
-
-	for (i = 0; i < N; ++i)
-		scanf("%d", city_hands + i);
-
-	int c_1, c_2;
-	int length;
-	result_hands[C1] = city_hands[C1];
-	for (i = 0; i < M; ++i){
-		scanf("%d%d%d", &c_1, &c_2, &length);
-		edge[c_1][c_2] = length;
-		edge[c_2][c_1] = length;
+	// 注意C1c1  C2c2区别，按照sb题目里面的，C为start end的编号，c就是点的通用编号
+	int c1, c2, L;
+	for (i = 0; i < M; ++i) {
+		scanf("%d %d %d", &c1, &c2, &L);
+		edges[c1][c2] = edges[c2][c1] = L;
 	}
 
-	///////////////////////////////////////////////////////////////////
 
-	// modified dijkstra
+
+	// 下面全是dijkstra
+	for (i = 0; i < N; ++i) {
+		is_in_set[i] = false;
+		path_counts[i] = 0;
+		path_dists[i] = INT_MAX;
+		path_num_teams[i] = 0;
+	}
+
+	// 把C1源点放入set
 	int cur = C1;
-	// ad C1
-	dist[C1] = 0;
-	is_in_S[cur] = true;  
-	result_count[C1] = 1;
-	result_hands[C1] = city_hands[C1];
-	int min_dist;
+	is_in_set[C1] = true;
+	path_counts[C1] = 1;
+	path_dists[C1] = 0;
+	path_num_teams[C1] = num_teams[C1];
+
+	// 松弛路径后挑选最短路径
+	int min_dist, min_index;
+
+	// 其他变量
+	int cur_dist, cur_num_teams;
+
 	for (i = 1; i < N; ++i) {
-		for (j = 0; j < N; ++j)
-			if (edge[cur][j] != INT_MAX && !is_in_S[j]) {
-				int new_dist = dist[cur] + edge[cur][j];
-				if (dist[j] > new_dist) {
-					dist[j] = new_dist;
-					result_hands[j] = result_hands[cur] + city_hands[j];
-					result_count[j] = result_count[cur];
+		// 松弛cur邻接点的路径
+		for (h = 0; h < N; ++h)
+			if (!is_in_set[h] && edges[h][cur] != INT_MAX) {
+				// 若h以cur为前缀节点，计算对应参数
+				cur_dist = path_dists[cur] + edges[h][cur];
+				cur_num_teams = path_num_teams[cur] + num_teams[h];
+
+				if (cur_dist < path_dists[h]) {
+					path_dists[h] = cur_dist;
+					path_num_teams[h] = cur_num_teams;
+					// 长为cur_dist从C1到h的路径条数，等于C1到cur的最短路径条数
+					path_counts[h] = path_counts[cur];
 				}
-				else if (dist[j] == new_dist)
-				{
-					result_hands[j] = max(result_hands[j], result_hands[cur] + city_hands[j]);
-					result_count[j] += result_count[cur];
+				// 有相同长度的路径
+				else if (cur_dist == path_dists[h]) {
+					// 长为cur_dist从C1到h的路径条数，等于原来的数据，加上C1到cur的最短路径条数
+					path_counts[h] += path_counts[cur];
+					if(cur_num_teams > path_num_teams[h])
+						path_num_teams[h] = cur_num_teams;
 				}
 			}
 
+		// 找最小值，放入S
 		min_dist = INT_MAX;
-		cur = -1;
-		for (j = 0; j < N; ++j)
-			if (!is_in_S[j] && dist[j] < min_dist)
-			{
-				min_dist = dist[j];
-				cur = j;
+		for (h = 0; h < N; ++h)
+			if (!is_in_set[h] && path_dists[h] < min_dist) {
+				min_dist = path_dists[h];
+				min_index = h;
 			}
-		is_in_S[cur] = true;
+
+		// 放入S
+		is_in_set[min_index] = true;
+		cur = min_index;
 	}
 
+	printf("%d %d", path_counts[C2], path_num_teams[C2]);
 
-	printf("%d %d", result_count[C2], result_hands[C2]);
 	system("pause");
 	return 0;
 }
