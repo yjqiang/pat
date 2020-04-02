9ae69b75a41aa512344d709c9e8015d1eb177c3d
yjqiang <yjqiang1@gmail.com>
2020-04-02 23:21:45 +0800

    a1004.1 调整代码（我是咋写出have_leaf = true？）

--- A/1004/main_1.cpp
+++ A/1004/main_1.cpp
@@ -1,54 +1,72 @@
-# define _CRT_SECURE_NO_WARNINGS
-# include <cstring>
-# include <cstdio>
-# include <cstdlib>
-# include <iostream>
-# include <algorithm>
-# include <climits>
-# include <queue>
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-#define MAX_N 101
+#define N_MAX 100
+#define ROOT 1
 
-bool edges[MAX_N][MAX_N] = {false};
-int counts[MAX_N] = { 0 };
-int max_level = -1;  // 0st, 1st,2rd ...
+struct Node {
+	int ID;
+	vector<int> Child_IDs;
+};
 
-void func(int root, int level) {
+// nodes[ID] = 对应node
+Node nodes[N_MAX];
+
+// 每一层的叶子节点数目(root第0层)
+int leaf_nums_per_level[200] = { 0 };
+int max_level = -1;
+
+void dfs(int r, int level) {
+	// 更新树高
 	max_level = max(max_level, level);
-	int i;
-	bool have_leaf = false;
-	for (i = 0; i < MAX_N; ++i)
-		if (edges[root][i]) {
-			have_leaf = true;
-			func(i, level + 1);
-		}
 
-	if (!have_leaf)
-		++counts[level];
+	int n_child = nodes[r].Child_IDs.size();
+	// r节点无child，则其为叶子节点
+	if (n_child == 0)
+		++leaf_nums_per_level[level];
+	int i;
+	for (i = 0; i < n_child; ++i)
+		dfs(nodes[r].Child_IDs[i], level + 1);
 }
 
-int main(){
+int main() {
 	int N, M;
 	scanf("%d%d", &N, &M);
 	
-	if (!N)
-		return 0;
-
-	int i, k;
-	int ID, K, child;
+	int i, h;
+	int ID, K, Child_ID;
 	for (i = 0; i < M; ++i) {
 		scanf("%d%d", &ID, &K);
-		for (k = 0; k < K; ++k) {
-			scanf("%d", &child);
-			edges[ID][child] = true;
+		for (h = 0; h < K; ++h) {
+			scanf("%d", &Child_ID);
+			nodes[ID].Child_IDs.push_back(Child_ID);
 		}
 	}
-	func(1, 0);
-	printf("%d", counts[0]);
-	for (i = 1; i <= max_level; i++)
-		printf(" %d", counts[i]);
+
+	dfs(ROOT, 0);
+	
+	for (i = 0; i <= max_level; ++i) {
+		if (i == 0)
+			printf("%d", leaf_nums_per_level[i]);
+		else
+			printf(" %d", leaf_nums_per_level[i]);
+	}
+
+
 
 	system("pause");
 	return 0;
