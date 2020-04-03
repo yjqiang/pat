115c43c01bf548c3256ad5a1d6f7d3e9f1b30ee4
yjqiang <yjqiang1@gmail.com>
2020-04-03 22:36:24 +0800

    本质为subset

--- A/1053/main_1.cpp
+++ A/1053/main_1.cpp
@@ -7,95 +7,74 @@
 #include <iostream>
 #include <map>
 #include <cmath>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
+#define N_MAX 100
+int N, M, S;
+
 struct Node {
 	int weight;
-	// ID 对应了nodes数组的index
-	vector<int> children_IDs;
+	vector<int> Child_IDs;
 };
 
-#define N_MAX 100
+// nodes[ID] = 对应node
 Node nodes[N_MAX];
 
+vector<vector<int>>results;
+vector<int>tmp;
+void subset(int cur_ID, int sum_weight) {
+	int cur_weight = nodes[cur_ID].weight;
+	sum_weight += cur_weight;
+	tmp.push_back(cur_weight);
 
+	int child_num = nodes[cur_ID].Child_IDs.size();
 
-// 目标S
-int S;
-int result = 0;
-
-// 暂存root到当前节点的路径，上的所有经过的点
-vector<int> tmp;
-vector<vector<int>> results;
-
-// root(ID:00)->...->当前节点的父节点的路径和为sum
-void pre(int parent_ID, int sum) {
-	sum += nodes[parent_ID].weight;
-	tmp.push_back(nodes[parent_ID].weight);
+	if (child_num == 0) {
+		if (sum_weight == S)
+			results.push_back(tmp);
+		tmp.pop_back();
+		return;
+	}
 
 	int i;
-	int n = nodes[parent_ID].children_IDs.size();
-
-	// 为叶节点，就判断一下
-	if (n == 0 && sum == S)
-		results.push_back(tmp);
-
-
-	for (i = 0; i < n; ++i)
-		pre(nodes[parent_ID].children_IDs[i], sum);
+	for (i = 0; i < child_num; ++i)
+		subset(nodes[cur_ID].Child_IDs[i], sum_weight);
 
 	tmp.pop_back();
 }
 
-
-// 从大到小排序
-// 这里不可能存在ab不等长且俩人有效元素一样的情况（即一个是另一个的前缀部分）这是题目约束的
-bool cmp(vector<int>& a, vector<int>& b) {
-	int n = min(a.size(), b.size());
-	int i;
-	for (i = 0; i < n; ++i) {
-		if (a[i] > b[i])
-			return true;
-		else if (a[i] < b[i])
-			return false;
-	}
-	return false;
-}
-
 int main() {
-	int N, M;
-	int ID, K, child_ID;
+	scanf("%d %d %d", &N, &M, &S);
 
 	int i, h;
-
-	scanf("%d %d %d", &N, &M, &S);
-	// id 从0->(N-1)
 	for (i = 0; i < N; ++i)
 		scanf("%d", &(nodes[i].weight));
 
+	int ID, Child_ID, K;
 	for (i = 0; i < M; ++i) {
 		scanf("%d %d", &ID, &K);
 		for (h = 0; h < K; ++h) {
-			scanf("%d", &child_ID);
-			nodes[ID].children_IDs.push_back(child_ID);
+			scanf("%d", &Child_ID);
+			nodes[ID].Child_IDs.push_back(Child_ID);
 		}
 	}
-	int sum = 0;
-	pre(0, sum);
 
-	sort(results.begin(), results.end(), cmp);
+	subset(0, 0);
 
-	int n_results, n;
-	n_results = results.size();
-	for (i = 0; i < n_results; ++i) {
+	sort(results.begin(), results.end());
+	for (i = results.size() - 1; i >= 0; --i) {
 		printf("%d", results[i][0]);
-		n = results[i].size();
-		for (h = 1; h < n; ++h)
+		for (h = 1; h < results[i].size(); ++h)
 			printf(" %d", results[i][h]);
 		printf("\n");
 	}
-		
+
 	system("pause");
 	return 0;
 }
