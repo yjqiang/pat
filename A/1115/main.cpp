e9da538221589ca570e87223f4d8721ca1f31bbc
yjqiang <yjqiang1@gmail.com>
2020-04-05 22:36:50 +0800

    可能就一层树

--- A/1115/main.cpp
+++ A/1115/main.cpp
@@ -9,51 +9,40 @@
 #include <cmath>
 #include <climits>
 #include <queue>
+#include <stack>
 #include <set>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-#define N_MAX 10000
+#define N_MAX 1000
 
 struct Node {
 	int value;
-	Node* left,* right; // NULL表示无
+	Node* left, * right;
 };
 
 
-// 指针引用
-void myInsert(Node* & root, int num) {
-	if (root == NULL) {
-		root = (Node*)malloc(sizeof(Node));
-		root->value = num;
-		root->left = NULL;
-		root->right = NULL;
-		return;
-	}
-
-	if (num <= root->value)
-		myInsert(root->left, num);
-	else
-		myInsert(root->right, num);
-
-}
-
-// bfs中每层的节点个数
-vector<int> counts_per_level;
-
 void bfs(Node* root) {
-	queue<Node*> q;
+	queue<Node*>q;
 	q.push(root);
 
-	int count_per_level;
+	int i, h;
 	Node* cur;
-	int i;
-	while (!q.empty()) {
-		// 本层的节点数目
-		count_per_level = q.size();
+	// n1表示倒数第一层，n2表示倒数第二层
+	int n1, n2;
+	int nodes_num_per_level;
 
-		for (i = 0; i < count_per_level; ++i) {
+	// n1 = n2 = -1;
+	n1 = n2 = 0;
+	while (!q.empty()) {
+		nodes_num_per_level = q.size();
+		// 层数加一，上一层的n1现在成了本层的n2
+		n2 = n1;
+		n1 = nodes_num_per_level;
+
+		for (i = 0; i < nodes_num_per_level; ++i) {
 			cur = q.front();
 			q.pop();
 
@@ -63,31 +52,43 @@ void bfs(Node* root) {
 				q.push(cur->right);
 		}
 
-		counts_per_level.push_back(count_per_level);
-
 	}
+	// 这树可能就一层
+	//if (n2 == -1)
+	//	exit(-1);
+	printf("%d + %d = %d", n1, n2, n1 + n2);
+}
+
+
+void insert(int value, Node*& cur) {
+	if (cur == NULL) {
+		cur = (Node*)malloc(sizeof(Node));
+		cur->value = value;
+		cur->left = cur->right = NULL;
+		return;
+	}
+	if (value <= cur->value)
+		insert(value, cur->left);
+	else
+		insert(value, cur->right);
 }
 
 int main() {
 	int N;
 	scanf("%d", &N);
-	
-	Node* root = NULL;
 
-	int num;
-	int i;
+	int i, h;
+	int value;
+
+	Node* root = NULL;
 	for (i = 0; i < N; ++i) {
-		scanf("%d", &num);
-		myInsert(root, num);
+		scanf("%d", &value);
+
+		insert(value, root);
 	}
-	
+
 	bfs(root);
 
-	int n_size = counts_per_level.size();
-	int n1 = counts_per_level[n_size - 1];
-	int n2 = counts_per_level[n_size - 2];
-	printf("%d + %d = %d", n1, n2, n1 + n2);
-	
 	system("pause");
 	return 0;
 }
