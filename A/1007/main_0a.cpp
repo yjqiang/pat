44bd8936b9262a5dab671c50e45e47c48096e87d
yjqiang <yjqiang1@gmail.com>
2020-04-08 23:38:11 +0800

    等价替换

--- A/1007/main_0a.cpp
+++ A/1007/main_0a.cpp
@@ -1,47 +1,86 @@
-# define _CRT_SECURE_NO_WARNINGS
-# include <cstdlib>
-# include <cstdio>
-# include <vector>
-# include <ctime>
-# include <iostream>
-# include <cstring>
-# include <climits>
-# include <cmath>
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-int a[10005];
+#define K_MAX 10001
+int K;
+
+int nums[K_MAX];
+
+bool allNegative() {
+	int i;
+	for (i = 1; i <= K; ++i)
+		if (nums[i] >= 0)
+			return false;
+	return true;
+}
+
+// result = nums[i] + nums[i+1] + ... + nums[j]
+struct Sum {
+	int result;
+	int i, j;
+};
 
 int main() {
-	int K;
+	
 	scanf("%d", &K);
 
 	int i;
-	for (i = 0; i < K; ++i)
-		scanf("%d", a + i);
+	for (i = 1; i <= K; ++i)
+		scanf("%d", nums + i);
 
-	int sum = 0;
-	int min_sum = 0, min_sum_right = -1;
-	int left = 0, right = K-1;
-	int dp_i = INT_MIN;
-
-	for (i = 0; i < K; ++i) {
-		sum += a[i];
-		if (sum - min_sum > dp_i) {
-			dp_i = sum - min_sum;
-			left = min_sum_right + 1;
-			right = i;
-		}
-		if (sum < min_sum) {
-			min_sum = sum;
-			min_sum_right = i;
-		}
-		// printf("i=%d, sum=%d, min_sum=%d, min_sum_right=%d, dp_i=%d, left=%d, right=%d\n", i, sum, min_sum, min_sum_right, dp_i, left, right);
+	if (allNegative()) {
+		printf("0 %d %d", nums[1], nums[K]);
+		return 0;
 	}
-	if(dp_i < 0)
-		printf("%d %d %d", 0, a[0], a[K-1]);
-	else
-		printf("%d %d %d", dp_i, a[left], a[right]);
+
+	// min_sums[i] = nums[1] + nums[2] + ... + nums[i]  前缀和最小的情况
+	Sum min_sums_i;
+	min_sums_i.result = 0;
+	min_sums_i.i = 1;  // 这个是写死的，不变了
+	min_sums_i.j = 0;  // min_sums[0] = 0
+
+	// sums[i] = nums[1] + nums[i + 1] + ... + nums[i]  前缀和
+	int sums_i;
+
+	// result_sum 为最优子串的结果
+	Sum result;
+	result.result = INT_MIN;
+	result.i = result.j = 1;
+
+
+	sums_i = 0;
+	for (i = 1; i <= K; ++i) {
+		sums_i += nums[i];
+
+		if (sums_i - min_sums_i.result > result.result) {
+			result.result = sums_i - min_sums_i.result;
+			result.i = min_sums_i.j + 1;
+			result.j = i;
+		}
+
+		if (sums_i < min_sums_i.result) {
+			min_sums_i.result = sums_i;
+			min_sums_i.j = i;
+		}
+	}
+
+	printf("%d %d %d", result.result, nums[result.i], nums[result.j]);
+
 	system("pause");
 	return 0;
 }
