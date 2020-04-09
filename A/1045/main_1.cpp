9485b2e373a2574afba0f702353f3371f4104b95
yjqiang <yjqiang1@gmail.com>
2020-04-09 19:39:27 +0800

    等价代换

--- A/1045/main_1.cpp
+++ A/1045/main_1.cpp
@@ -1,61 +1,72 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <vector>
 #include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 #define N_MAX 201
-#define M_MAX 201
-#define L_MAX 10001
+#define M_MAX 200
+#define L_MAX 10000
 
-// 把花里胡哨的颜色还原为favor里面的对应index
-int book[N_MAX] = {0};
-int a[L_MAX];
+// 把输入的第一行数组的元素值映射为输入顺序，这样就把题目转化为最长不降子串
+// 默认为 0，表示没这个数
+int books[N_MAX] = { 0 };
 
-// dp[i]表示以i为末尾的最大单调不降子序列
+int nums[L_MAX];
+
+// dp[i]表示以i为结尾的最长不降子串的长度
 int dp[L_MAX];
 
 int main() {
-	// 这道题 5 5 1 2 3 4 5 7 1 1 1 1 1 1 1  输出7，是个sb题目
-	int N;
-	scanf("%d", &N);
+	int N, M, L;
 
-	int M;
-	scanf("%d", &M);
+	scanf("%d %d", &N, &M);
 
-	int i;
+	int i, h;
 	int num;
-	// favor 颜色unique
 	for (i = 1; i <= M; ++i) {
 		scanf("%d", &num);
-		// 用于把“L”个颜色对应回index，0缺省；这样题目就成了找最大单调不降子序列
-		book[num] = i;
+		books[num] = i;
 	}
 
-	int n_a = 0;
-	int L;
 	scanf("%d", &L);
-	for (i = 0; i < L; ++i) {
+	for (i = 0, h = 0; i < L; ++i) {
 		scanf("%d", &num);
-		// 过滤掉不在favor里面的颜色
-		if (book[num] > 0) {
-			a[n_a] = book[num];
-			++n_a;
-		}
+		// 过滤掉那些不在需要序列里面的元素
+		if (books[num])
+			nums[h++] = books[num];
 	}
+	// 更新L长度
+	L = h;
+
+
 
 	int result = -1;
-	int j;
-	for (i = 0; i < n_a; ++i) {
-		dp[i] = 1;
-		for (j = 0; j < i; ++j)
-			// 可以在dp[j] 对应的子序列上扩充
-			if (a[i] >= a[j])
-				dp[i] = max(dp[i], dp[j] + 1);
+
+	// 每个循环都是在求dp[i]
+	for (i = 0; i < L; ++i) {
+		dp[i] = 1; // 因为最差就是这个子串只有本身一个元素
+		for (h = 0; h < i; ++h)
+			// 可以在dp[h]对应子串的基础上，继续扩展
+			if (nums[i] >= nums[h])
+				dp[i] = max(dp[i], dp[h] + 1);
+
 		result = max(dp[i], result);
 	}
-	printf("%d", result);
 
+	printf("%d", result);
 	system("pause");
 	return 0;
 }
