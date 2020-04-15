44875cdf605747c95be8933e0389afc8a6067620
yjqiang <yjqiang1@gmail.com>
2020-04-15 21:31:41 +0800

    等价代换

--- A/1033/main_1.cpp
+++ A/1033/main_1.cpp
@@ -1,104 +1,119 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-
-#include <map>
-
-#include <set>
-
-#include <climits>
+#include <vector>
+#include <algorithm>
 #include <cstdio>
-#include <queue>
+#include <cstring>
 #include <string>
-#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
+#define N_MAX 502
+
 struct GasStation {
-	double Pi;
-	int Di;
-	int end;
+	double Pi;  // 汽油的单价
+	int Di;  // 距离起点的距离
 };
-#define N_MAX 500
+
 GasStation gas_stations[N_MAX];
 
+// 按照距离远近排序
 int cmp(const void* p0, const void* p1) {
-	GasStation* gas_station0 = (GasStation*)p0;
-	GasStation* gas_station1 = (GasStation*)p1;
-
-	return gas_station0->Di - gas_station1->Di;
+	GasStation* a = (GasStation*)p0;
+	GasStation* b = (GasStation*)p1;
+	return a->Di - b->Di;
 }
 
 int main() {
 	int C_max, D, D_avg, N;
-	scanf("%d%d%d%d", &C_max, &D, &D_avg, &N);
+	scanf("%d %d %d %d", &C_max, &D, &D_avg, &N);
 
-	int i;
-	double Pi;
-	int Di;
-	for (i = 0; i < N; ++i) {
-		scanf("%lf%d", &Pi, &Di);
-		gas_stations[i].Pi = Pi;
-		gas_stations[i].Di = Di;
-		gas_stations[i].end = Di + C_max * D_avg;
-	}
+	// 装满油箱后最远跑多少距离
+	int D_max = C_max * D_avg;
 
-	// 用于终止操作
-	gas_stations[i].Pi = -1;
+	int i, h;
+	int num;
+	for (i = 1; i <= N; ++i)
+		scanf("%lf %d", &gas_stations[i].Pi, &gas_stations[i].Di);
+
+	// 按照距离远近排序
+	qsort(gas_stations + 1, N, sizeof(GasStation), cmp);
+	// 终止符号
+	++N;
+	gas_stations[N].Pi = -1;
 	gas_stations[i].Di = D;
-	qsort(gas_stations, N, sizeof(GasStation), cmp);
 
+
+	// 当前gas的量
+	double cur_gas = 0;
 
 	double cost = 0;
-	int cur_gas_station_index;
-	if (gas_stations[0].Di != 0) {
-		printf("The maximum travel distance = 0.00");
+
+	int max_Di = 0;
+	int next_station_index;
+	double next_station_Pi;
+
+
+	if (gas_stations[1].Di != 0) {
+
+		printf("The maximum travel distance = %.2lf", (double)max_Di);
+		system("pause");
 		return 0;
 	}
 
-	int min_index;
-	double min_element;
-	double tank_needed_from_cur2next;
-	double remain_tank;
+	// i 开始是选中了 1号加油站，当i到达N（人为添加了 GasStation{-1, D} 后）时，说明到达终点
+	for (i = 1; i < N;) {
+		next_station_index = -1;
+		next_station_Pi = INT_MAX;
 
-	for (cur_gas_station_index = 0, remain_tank = 0; cur_gas_station_index < N; ) {
-		min_element = -1;
-		min_index = -1;
-		// 注意 i <= N 等于号
-		for (i = cur_gas_station_index + 1; i <= N && gas_stations[cur_gas_station_index].end >= gas_stations[i].Di; ++i) {
-			// <= 等于号不能去掉，若所有加油站价格都比现在贵，要在此处加满油之后到达价格最低处，若多个最低处的地点，要到最远点
-			if (min_element == -1 || gas_stations[i].Pi <= min_element) {
-				min_element = gas_stations[i].Pi;
-				min_index = i;
-			}
-			if (min_element < gas_stations[cur_gas_station_index].Pi)
+
+		// 在最大范围（油箱加满跑路距离）内寻找next station
+		max_Di = gas_stations[i].Di + D_max;
+		for (h = i + 1; h <= N && gas_stations[h].Di <= max_Di; ++h) {
+			// 有加油站更便宜，加一点油正好去这个加油站
+			if (gas_stations[h].Pi < gas_stations[i].Pi) {
+				next_station_index = h;
+				next_station_Pi = gas_stations[h].Pi;
 				break;
+			}
+			// 否则尽量找一个最便宜的加油站，加满油，到这里去(等于号不能去掉，两加油站价格一样，选远的，因为i处便宜，尽量跑远一点划算)
+			else if (gas_stations[h].Pi <= next_station_Pi) {
+				next_station_index = h;
+				next_station_Pi = gas_stations[h].Pi;
+			}
 		}
 
-		if (min_index == -1) {
-			printf("The maximum travel distance = %.2lf", (double)gas_stations[cur_gas_station_index].end);
+		// 方圆十里没有加油站，到不了终点了
+		if (next_station_index == -1) {
+			printf("The maximum travel distance = %.2lf", (double)max_Di);
+			system("pause");
 			return 0;
 		}
-		tank_needed_from_cur2next = ((double)gas_stations[min_index].Di - gas_stations[cur_gas_station_index].Di) / D_avg;
-		if (min_element < gas_stations[cur_gas_station_index].Pi) {
-
-			// tank_needed_from_cur2next - remain_tank 一定大于等于0，上一加油站加的油到min_index一定不够，
-			// 否则min_index处于上一点的end范围内，就会从事上一点的迭代比较操作，若cur的油价小于上一代，则上一代的油恰好到cur；
-			// 否则上一加油站的迭代选择中，应该选min_index，而不是cur_gas_station_index
-			cost += (tank_needed_from_cur2next - remain_tank) * gas_stations[cur_gas_station_index].Pi;
-			remain_tank = 0;
+		// 有加油站更便宜，加一点油正好去这个加油站（两加油站Pi相等即 next_station_Pi == gas_stations[i].Pi 的时候，加到满/加一点均可）
+		if (next_station_Pi < gas_stations[i].Pi) {
+			// (距离差 / 单位油耗 - 邮箱剩余) * 单价
+			cost += (((double)gas_stations[next_station_index].Di - gas_stations[i].Di) / D_avg - cur_gas) * gas_stations[i].Pi;
+			// 到达该加油站后，无余量了
+			cur_gas = 0;
 		}
+		// 加满去该地
 		else {
-			cost += (C_max - remain_tank) * gas_stations[cur_gas_station_index].Pi;
-			remain_tank = C_max - tank_needed_from_cur2next;
+			cost += ((double)C_max - cur_gas) * gas_stations[i].Pi;
+			cur_gas = C_max - ((double)gas_stations[next_station_index].Di - gas_stations[i].Di) / D_avg;
 		}
 
-		cur_gas_station_index = min_index;
-
+		i = next_station_index;
+		// printf("%d\n", i);
 	}
-
-
 	printf("%.2lf", cost);
-
 	system("pause");
 	return 0;
 }
